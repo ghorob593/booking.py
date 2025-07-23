cd77ade161304e376abe8ef597fe5f4a2d377c7f
ghorob593 <andrijpirozenko893@gmail.com>
2025-07-23 20:00:35 +0300

    Delete core directory

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-app_name = 'core'
-
-urlpatterns = [
-    path('spaces/', views.SpaceListView.as_view(),name="space_list"),
-    path('spaces/<slug:slug>', views.SpaceListView.as_view(), name="space_detail" )
-    
-]
